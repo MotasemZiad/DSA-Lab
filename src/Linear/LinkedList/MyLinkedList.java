b3003c95818b51c9bc878c676a21242c6f0890a1
MotasemZiad <moatasem.abunema@gmail.com>
2022-11-14 19:38:23 +0200

    My Custom Linked List

--- src/Linear/LinkedList/MyLinkedList.java
+++ src/Linear/LinkedList/MyLinkedList.java
@@ -2,56 +2,165 @@ package Linear.LinkedList;
 
 import Linear.MyList;
 
-public class MyLinkedList implements MyList {
+public class MyLinkedList<T> implements MyList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
 
-    @Override
-    public void add(Object item) {
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
+    private boolean checkIndex(int index) {
+        return index >= 0 && index < size();
+    }
+
+    private Node<T> getNode(int index) {
+        if (!checkIndex(index))
+            return null;
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current;
     }
 
     @Override
-    public void add(int index, Object item) {
-
+    public void add(T item) {
+        if (item == null)
+            return;
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
-    public Object remove(int index) {
-        return null;
+    public void add(int index, T item) {
+        if (item == null)
+            return;
+        if (!checkIndex(index))
+            return;
+        Node<T> newNode = new Node<>(item);
+        if (index == 0) {
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            Node<T> prevNode = getNode(index - 1);
+            newNode.setNext(prevNode.getNext());
+            prevNode.setNext(newNode);
+        }
+        size++;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (!checkIndex(index))
+            return null;
+        T item;
+        if (index == 0) {
+            item = head.getElement();
+            head = head.getNext();
+        } else {
+            Node<T> prevNode = getNode(index - 1);
+            item = prevNode.getNext().getElement();
+            prevNode.setNext(prevNode.getNext().getNext());
+        }
+        size--;
+        return item;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
-    public int indexOf(Object item) {
-        return 0;
+    public int indexOf(T item) {
+        if (item == null)
+            return -1;
+        Node<T> currentNode = head;
+        int index = 0;
+        while (currentNode != null) {
+            if (currentNode.getElement().equals(item))
+                return index;
+            currentNode = currentNode.getNext();
+            index++;
+        }
+        return -1;
     }
 
     @Override
-    public void replace(int index, Object item) {
-
+    public void replace(int index, T item) {
+        if (item == null)
+            return;
+        if (!checkIndex(index))
+            return;
+        Node<T> currentNode = getNode(index);
+        currentNode.setElement(item);
     }
 
     @Override
-    public Object get(int index) {
-        return null;
+    public T get(int index) {
+        if (!checkIndex(index))
+            return null;
+        return getNode(index).getElement();
     }
 
     @Override
-    public boolean contains(Object item) {
-        return false;
+    public boolean contains(T item) {
+        return indexOf(item) != -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
+    private class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+        }
+
+        public Node(T element, Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public void setElement(T element) {
+            this.element = element;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
 }
