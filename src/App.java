7027771c72c6b011721a25cd04fa0e027ccb1c8c
MotasemZiad <moatasem.abunema@gmail.com>
2022-11-14 18:52:34 +0200

    using generics

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        Integer i = (Integer) list.iterator().next();
+        System.out.println(i);
+
     }
 }
--- src/Linear/LinkedList/MyLinkedList.java
+++ src/Linear/LinkedList/MyLinkedList.java
@@ -6,61 +6,51 @@ public class MyLinkedList implements MyList {
 
     @Override
     public void add(Object item) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void add(int index, Object item) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public Object remove(int index) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public int indexOf(Object item) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public void replace(int index, Object item) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public Object get(int index) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean contains(Object item) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
         return false;
     }
 
