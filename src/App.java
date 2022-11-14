9708d187e2bbe755189e2ec963fe2783467096e1
MotasemZiad <moatasem.abunema@gmail.com>
2022-11-14 19:27:09 +0200

    exploring the matrix array

--- src/App.java
+++ src/App.java
@@ -8,6 +8,64 @@ public class App {
         System.out.println("The second largest number is: " + getSecondLargest(arrayOfNumbers));
         System.out.println("The second smallest number is: " + getSecondSmallest(arrayOfNumbers));
 
+        int[][] matrix2D = {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 }
+        };
+
+        System.out.println("The Left Diagonal of the matrix is: ");
+        printLeftDiagonal(matrix2D);
+        System.out.println("The Right Diagonal of the matrix is: ");
+        printRightDiagonal(matrix2D);
+
+        System.out.println("The difference between the sum of the left diagonal and the right diagonal is: "
+                + getDiagonalDifference(matrix2D));
+
+    }
+
+    public static void printLeftDiagonal(int[][] matrix2D) {
+        for (int i = 0; i < matrix2D.length; i++) {
+            for (int j = 0; j < matrix2D[i].length; j++) {
+                if (i == j) {
+                    System.out.print(matrix2D[i][j] + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printRightDiagonal(int[][] matrix2D) {
+        for (int i = 0; i < matrix2D.length; i++) {
+            for (int j = 0; j < matrix2D[i].length; j++) {
+                if (i + j == matrix2D.length - 1) {
+                    System.out.print(matrix2D[i][j] + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // This method returns the difference between the left and the right diagonals
+    // of a given matrix
+    public static int getDiagonalDifference(int[][] matrix2D) {
+        int leftDiagonal = 0;
+        int rightDiagonal = 0;
+
+        for (int i = 0; i < matrix2D.length; i++) {
+            for (int j = 0; j < matrix2D[i].length; j++) {
+                if (i == j) {
+                    leftDiagonal += matrix2D[i][j];
+                }
+                if (i + j == matrix2D.length - 1) {
+                    rightDiagonal += matrix2D[i][j];
+                }
+            }
+        }
+
+        return Math.abs(leftDiagonal - rightDiagonal);
     }
 
     public static int getSum(int[] arr) {
