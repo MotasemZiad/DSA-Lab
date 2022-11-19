acb5bb0464d9adc70114e6c9d85d0aa718a1da28
MotasemZiad <miziad2000@gmail.com>
2022-11-19 12:26:50 +0200

    some updates

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
+import Linear.LinkedList.MyLinkedList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
     }
 
     public static void printLeftDiagonal(int[][] matrix2D) {
